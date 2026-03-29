44fe6e3897c7d04241d830dfb1b0fbf232f8d8a4
deema alfazza <alideema694@gmail.com>
2026-03-30 00:21:22 +0300

    Add project files.

---
+++ assigment2/Program.cs
@@ -0,0 +1,20 @@
+﻿string word = "successfully";
+
+Dictionary<char, int> count = new Dictionary<char, int>();
+foreach (char str in word)
+{
+    if (count.ContainsKey(str))
+    {
+        count[str]++;
+    }
+    else
+    {
+        count.Add(str, 1);
+    }
+}
+
+Console.WriteLine($"the word is :{word}");
+foreach (var item in count)
+{
+    Console.WriteLine($"char:{item.Key}, count:{item.Value}");
+}43r
